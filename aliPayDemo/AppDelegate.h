00c59e970ad4a2fc921620ee39cc70139e31dfce
cesarewang <hq3595@gmail.com>
2015-04-29 16:54:18 +0800

    Initial Commit

---
+++ aliPayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aliPayDemo
+//
+//  Created by Cesare on 4/29/15.
+//  Copyright (c) 2015 Hand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
